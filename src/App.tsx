dd3f66c2185b99d67e9e88afa57431d231f9906a
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-16 23:23:58 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// DEPENDENCY
+import React from 'react'
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Redux Toolkit Login</h1>
+    </div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+// DEPENDENCY
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// COMPONENT
+import { App } from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
